2a7f50988c68f17956c6251352b2e71522e8a919
Andreas G <13819164+agaertner@users.noreply.github.com>
2022-09-25 01:20:46 +0200

    🎉add project

---
+++ src/Core/Controllers/AutoClickController.cs
@@ -0,0 +1,207 @@
+﻿using Blish_HUD;
+using Blish_HUD.Controls.Intern;
+using Blish_HUD.Input;
+using Microsoft.Xna.Framework.Audio;
+using Nekres.Inquest_Module.UI.Controls;
+using System;
+using ButtonState = Microsoft.Xna.Framework.Input.ButtonState;
+using Color = Microsoft.Xna.Framework.Color;
+using Mouse = Blish_HUD.Controls.Intern.Mouse;
+using Point = Microsoft.Xna.Framework.Point;
+namespace Nekres.Inquest_Module.Core.Controllers
+{
+    internal class AutoClickController : IDisposable
+    {
+        private float _soundVolume;
+        public float SoundVolume
+        {
+            get => Math.Min(GameService.GameIntegration.Audio.Volume, _soundVolume);
+            set => _soundVolume = value;
+        }
+
+        private KeyBinding AutoClickHoldKey => InquestModule.ModuleInstance.AutoClickHoldKeySetting.Value;
+        private KeyBinding AutoClickToggleKey => InquestModule.ModuleInstance.AutoClickToggleKeySetting.Value;
+
+        private SoundEffect[] _doubleClickSfx;
+        public SoundEffect DoubleClickSfx => _doubleClickSfx[RandomUtil.GetRandom(0, 2)];
+
+        private DateTime _nextHoldClick = DateTime.UtcNow;
+
+        private DateTime _nextToggleClick = DateTime.UtcNow;
+
+        private bool _paused;
+        private TimeSpan _pausedRemainingTime;
+
+        private int _toggleIntervalMs;
+        private System.Drawing.Point _togglePos;
+        private bool _toggleActive;
+        private Color _redShift;
+        private bool _inTogglePrompt;
+
+        private TaskIndicator _indicator;
+        private ClickIndicator _clickIndicator;
+
+        private bool _isDisposing;
+        public AutoClickController()
+        {
+            this.SoundVolume = 1;
+            _redShift = new Color(255, 57, 57);
+            AutoClickHoldKey.Enabled = true;
+            AutoClickHoldKey.Activated += OnHoldActivated;
+            AutoClickToggleKey.Enabled = true;
+            AutoClickToggleKey.Activated += OnToggleActivate;
+            _doubleClickSfx = new[]
+            {
+                InquestModule.ModuleInstance.ContentsManager.GetSound(@"audio\double-click-1.wav"),
+                InquestModule.ModuleInstance.ContentsManager.GetSound(@"audio\double-click-2.wav"),
+                InquestModule.ModuleInstance.ContentsManager.GetSound(@"audio\double-click-3.wav")
+            };
+            GameService.Gw2Mumble.PlayerCharacter.IsInCombatChanged += OnIsInCombatChanged;
+            GameService.GameIntegration.Gw2Instance.IsInGameChanged += OnIsInGameChanged;
+        }
+
+        private void OnIsInCombatChanged(object o, ValueEventArgs<bool> e)
+        {
+            if (e.Value) Deactivate();
+        }
+        private void OnIsInGameChanged(object o, ValueEventArgs<bool> e)
+        {
+            if (!e.Value) Deactivate();
+        }
+        private void OnHoldActivated(object o, EventArgs e)
+        {
+            if (_toggleActive) Deactivate();
+        }
+
+        private void CreateClickIndicator(bool attachToCursor, bool forceRecreation = false)
+        {
+            if (forceRecreation) Deactivate();
+            _clickIndicator ??= new ClickIndicator(attachToCursor)
+            {
+                Parent = GameService.Graphics.SpriteScreen,
+                Size = new Point(32, 32),
+                Location = new Point(GameService.Input.Mouse.Position.X - 14, GameService.Input.Mouse.Position.Y - 14),
+            };
+        }
+
+        private void OnToggleActivate(object o, EventArgs e)
+        {
+            if (_toggleActive || IsBusy())
+            {
+                Deactivate();
+                return;
+            }
+            _inTogglePrompt = true;
+            _togglePos = WindowUtil.GetCursorPosition(GameService.GameIntegration.Gw2Instance.Gw2WindowHandle);
+            CreateClickIndicator(false, true);
+            NumericInputPrompt.ShowPrompt(OnToggleInputPromptCallback, "Enter an interval in seconds:", InquestModule.ModuleInstance.AutoClickToggleInterval.Value);
+        }
+
+        private void OnToggleInputPromptCallback(bool confirmed, double input)
+        {
+            _inTogglePrompt = false;
+            if (!confirmed)
+            {
+                Deactivate();
+                return;
+            }
+            _toggleActive = true;
+            _toggleIntervalMs = Math.Min(300000, Math.Max(250, (int)(input * 1000)));
+            _nextToggleClick = DateTime.UtcNow;
+            InquestModule.ModuleInstance.AutoClickToggleInterval.Value = _toggleIntervalMs / 1000.0;
+
+            _indicator ??= new TaskIndicator(false)
+            {
+                Parent = GameService.Graphics.SpriteScreen,
+                Size = new Point(50, 50),
+                Location = new Point(_clickIndicator.Location.X + 25, _clickIndicator.Location.Y - 32),
+            };
+        }
+
+        private bool HoldIsTriggering() => (InquestModule.ModuleInstance.HoldKeyWithLeftClickEnabledSetting.Value
+            ? AutoClickHoldKey.IsTriggering && GameService.Input.Mouse.State.LeftButton == ButtonState.Pressed
+            : AutoClickHoldKey.IsTriggering) && !_inTogglePrompt && !_isDisposing;
+
+        public void Update()
+        {
+            if (_toggleActive)
+            {
+                var isBusy = IsBusy();
+
+                _clickIndicator.Paused = isBusy;
+                _indicator.Paused = isBusy;
+                _indicator.Visible = !isBusy;
+
+                if (isBusy)
+                {
+                    _nextToggleClick = DateTime.UtcNow.Add(_pausedRemainingTime);
+                    return;
+                }
+
+                var remainingTime = DateTime.UtcNow.Subtract(_nextToggleClick);
+                _indicator.Text = remainingTime.ToString(remainingTime.TotalSeconds > -1 ? @"\.ff" : remainingTime.TotalMinutes > -1 ? "ss" : @"m\:ss").TrimStart('0');
+                _indicator.TextColor = Color.Lerp(Color.White, _redShift, 1 + (float)remainingTime.TotalMilliseconds / _toggleIntervalMs);
+
+                if (DateTime.UtcNow <= _nextToggleClick) return;
+                _clickIndicator.LeftClick();
+                if (!InquestModule.ModuleInstance.AutoClickSoundDisabledSetting.Value) DoubleClickSfx.Play(SoundVolume, 0, 0);
+                Mouse.DoubleClick(MouseButton.LEFT, _togglePos.X, _togglePos.Y);
+                Mouse.Click(MouseButton.LEFT, _togglePos.X, _togglePos.Y); // WM_BUTTONDBLCLK (0x0203) jams message queue. Unjam with followup click.
+                _nextToggleClick = DateTime.UtcNow.AddMilliseconds(_toggleIntervalMs);
+            }
+            else if (HoldIsTriggering())
+            {
+                CreateClickIndicator(true);
+                if (DateTime.UtcNow <= _nextHoldClick || !GameService.GameIntegration.Gw2Instance.Gw2HasFocus) return;
+                _clickIndicator.LeftClick(40);
+                if (!InquestModule.ModuleInstance.AutoClickSoundDisabledSetting.Value) DoubleClickSfx.Play(SoundVolume, 0, 0);
+                var pos = WindowUtil.GetCursorPosition(GameService.GameIntegration.Gw2Instance.Gw2WindowHandle);
+                Mouse.DoubleClick(MouseButton.LEFT, pos.X, pos.Y);
+                Mouse.Click(MouseButton.LEFT, pos.X, pos.Y);
+                _nextHoldClick = DateTime.UtcNow.AddMilliseconds(50);
+            }
+            else if (!_inTogglePrompt)
+            {
+                Deactivate();
+            }
+        }
+
+        private bool IsBusy()
+        {
+            var isBusy = !GameService.GameIntegration.Gw2Instance.Gw2IsRunning
+                         || !GameService.GameIntegration.Gw2Instance.IsInGame
+                         || GameService.Gw2Mumble.UI.IsTextInputFocused
+                         || GameService.Input.Mouse.CameraDragging
+                         || GameService.Gw2Mumble.PlayerCharacter.IsInCombat;
+
+            if (isBusy)
+            {
+                if (_paused) return true;
+                _paused = true;
+                _pausedRemainingTime = _nextToggleClick.Subtract(DateTime.UtcNow);
+                return true;
+            }
+            if (_paused) _paused = false;
+            return false;
+        }
+
+        private void Deactivate()
+        {
+            _toggleActive = false;
+            _clickIndicator?.Dispose();
+            _clickIndicator = null;
+            _indicator?.Dispose();
+            _indicator = null;
+        }
+
+        public void Dispose()
+        {
+            _isDisposing = true;
+            AutoClickToggleKey.Activated -= OnToggleActivate;
+            GameService.Gw2Mumble.PlayerCharacter.IsInCombatChanged -= OnIsInCombatChanged;
+            GameService.GameIntegration.Gw2Instance.IsInGameChanged -= OnIsInGameChanged;
+            Deactivate();
+            foreach (var sfx in _doubleClickSfx) sfx?.Dispose();
+        }
+    }
+}
---
+++ src/InquestModule.cs
@@ -0,0 +1,201 @@
+﻿using Blish_HUD;
+using Blish_HUD.Controls;
+using Blish_HUD.Controls.Extern;
+using Blish_HUD.Graphics.UI;
+using Blish_HUD.Input;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Nekres.Inquest_Module.Core.Controllers;
+using Nekres.Inquest_Module.UI.Models;
+using Nekres.Inquest_Module.UI.Views;
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Gw2Sharp.Models;
+
+namespace Nekres.Inquest_Module
+{
+    [Export(typeof(Module))]
+    public class InquestModule : Module
+    {
+        internal static readonly Logger Logger = Logger.GetLogger(typeof(InquestModule));
+
+        internal static InquestModule ModuleInstance;
+
+        #region Service Managers
+
+        internal SettingsManager SettingsManager => ModuleParameters.SettingsManager;
+        internal ContentsManager ContentsManager => ModuleParameters.ContentsManager;
+        internal DirectoriesManager DirectoriesManager => ModuleParameters.DirectoriesManager;
+        internal Gw2ApiManager Gw2ApiManager => ModuleParameters.Gw2ApiManager;
+
+        #endregion
+
+        internal SettingEntry<KeyBinding> AutoClickHoldKeySetting;
+        internal SettingEntry<bool> HoldKeyWithLeftClickEnabledSetting;
+        internal SettingEntry<KeyBinding> AutoClickToggleKeySetting;
+        internal SettingEntry<bool> AutoClickSoundDisabledSetting;
+        internal SettingEntry<float> AutoClickSoundVolume;
+        internal SettingEntry<KeyBinding> JumpKeyBindingSetting;
+        internal SettingEntry<KeyBinding> DodgeKeyBindingSetting;
+        internal SettingEntry<KeyBinding> DodgeJumpKeyBindingSetting;
+
+        // Hidden Settings (Cache)
+        internal SettingEntry<double> AutoClickToggleInterval;
+
+        private CornerIcon _moduleIcon;
+
+        private AutoClickController _autoClickController;
+        private DateTime _nextDodgeJump = DateTime.UtcNow;
+
+        [ImportingConstructor]
+        public InquestModule([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters)
+        {
+            ModuleInstance = this;
+        }
+
+        protected override void DefineSettings(SettingCollection settings)
+        {
+            var hotkeys = settings.AddSubCollection("Control Options", true, false);
+
+            AutoClickHoldKeySetting = hotkeys.DefineSetting("autoClickHoldKeyBinding", new KeyBinding(Keys.OemComma), 
+                () => "Hold Double Clicking", 
+                () => "Perform Double Clicks at the current cursor position while the key is being pressed.");
+            AutoClickToggleKeySetting = hotkeys.DefineSetting("autoClickToggleKeyBinding", new KeyBinding(Keys.OemOpenBrackets),
+                () => "Toggle Double Clicking",
+                () => "Perform Double Clicks in an interval at the position of the cursor at the time of pressing the key.");
+            DodgeJumpKeyBindingSetting = hotkeys.DefineSetting("dodgeJumpKeyBinding", new KeyBinding(ModifierKeys.Ctrl, Keys.Space),
+                () => "Dodge-Jump",
+                () => "Perform a dodge roll and a jump simultaneously.");
+
+            var controlOptions = hotkeys.AddSubCollection("Movement Keys to Trigger on Dodge-Jump", true, false);
+            DodgeKeyBindingSetting = controlOptions.DefineSetting("dodgeKeyBinding", new KeyBinding(Keys.V), () => "Dodge", () => "Do an evasive dodge roll, negating damage, in the direction your character is moving (backward if stationary).");
+            JumpKeyBindingSetting = controlOptions.DefineSetting("jumpKeyBinding", new KeyBinding(Keys.Space), () => "Jump", () => "Press to jump over obstacles.");
+
+            HoldKeyWithLeftClickEnabledSetting = hotkeys.DefineSetting("holdKeyWithLeftClick", false,
+                () => "Hold Double Clicking + Left Mouse Button",
+                () => "Perform Double clicks at the current cursor position while Hold Double Clicking and Left Mouse Button is being pressed.");
+
+            var audio = settings.AddSubCollection("Sound Options", true, false);
+            AutoClickSoundDisabledSetting = audio.DefineSetting("autoClickSoundsDisabled", false, 
+                () => "Disable Clicking Sounds", 
+                () => "Disables the sound alert when an auto click is performed.");
+
+            AutoClickSoundVolume = audio.DefineSetting("autoClickSoundsVolume", 80f,
+                () => "Clicking Sounds Volume", 
+                () => "Sets the audio volume of the clicking alerts.");
+
+            var hiddenSettingsCache = settings.AddSubCollection("hiddenSettingsCache", false, false);
+            AutoClickToggleInterval = hiddenSettingsCache.DefineSetting("autoClickToggleInterval", 0.0);
+        }
+
+        protected override void Initialize()
+        {
+            _autoClickController = new AutoClickController();
+            _autoClickController.SoundVolume = AutoClickSoundVolume.Value / 1000;
+
+            /*_moduleIcon = new CornerIcon
+            {
+                IconName = "Inquest Chipset",
+                Icon = ContentsManager.GetTexture("assault_cube_icon.png"),
+                Priority = "Inquest Chipset".GetHashCode()
+            };*/
+            JumpKeyBindingSetting.Value.Enabled = false;
+            DodgeKeyBindingSetting.Value.Enabled = false;
+            DodgeJumpKeyBindingSetting.Value.Enabled = true;
+            ToggleDodgeJump(GameService.Gw2Mumble.PlayerCharacter.CurrentMount == MountType.None);
+            AutoClickToggleKeySetting.Value.Enabled = true;
+            AutoClickHoldKeySetting.Value.Enabled = true;
+            AutoClickSoundVolume.SettingChanged += OnAutoClickSoundVolumeSettingChanged;
+            GameService.Gw2Mumble.PlayerCharacter.CurrentMountChanged += OnCurrentMountChanged;
+            GameService.Gw2Mumble.UI.IsTextInputFocusedChanged += OnIsTextInputFocusChanged;
+            GameService.Gw2Mumble.UI.IsMapOpenChanged += OnIsMapOpenChanged;
+            GameService.GameIntegration.Gw2Instance.IsInGameChanged += OnIsInGameChanged;
+        }
+        public override IView GetSettingsView()
+        {
+            return new CustomSettingsView(new CustomSettingsModel(SettingsManager.ModuleSettings));
+        }
+
+        protected override async Task LoadAsync()
+        {
+
+        }
+
+        private void OnAutoClickSoundVolumeSettingChanged(object o, ValueChangedEventArgs<float> e)
+        {
+            _autoClickController.SoundVolume = e.NewValue / 1000;
+        }
+
+        private void OnDodgeJumpKeyActivated(object o, EventArgs e)
+        {
+            if (DateTime.UtcNow < _nextDodgeJump) return;
+
+            if (DodgeKeyBindingSetting.Value.PrimaryKey == DodgeJumpKeyBindingSetting.Value.PrimaryKey 
+                && DodgeKeyBindingSetting.Value.ModifierKeys == DodgeJumpKeyBindingSetting.Value.ModifierKeys)
+            {
+                ScreenNotification.ShowNotification("Endless Loop Error. Dodge-Jump key cannot be the same as Dodge.", ScreenNotification.NotificationType.Error);
+                return;
+            }
+
+            if (JumpKeyBindingSetting.Value.PrimaryKey == DodgeJumpKeyBindingSetting.Value.PrimaryKey
+                && JumpKeyBindingSetting.Value.ModifierKeys == DodgeJumpKeyBindingSetting.Value.ModifierKeys)
+            {
+                ScreenNotification.ShowNotification("Endless Loop Error. Dodge-Jump key cannot be the same as Jump.", ScreenNotification.NotificationType.Error);
+                return;
+            }
+            _nextDodgeJump = DateTime.UtcNow.AddMilliseconds(80);
+            
+            if (DodgeKeyBindingSetting.Value.PrimaryKey != Keys.None)
+                Blish_HUD.Controls.Intern.Keyboard.Stroke((VirtualKeyShort)DodgeKeyBindingSetting.Value.PrimaryKey);
+            if (JumpKeyBindingSetting.Value.PrimaryKey != Keys.None)
+                Blish_HUD.Controls.Intern.Keyboard.Stroke((VirtualKeyShort)JumpKeyBindingSetting.Value.PrimaryKey);
+        }
+
+        private void OnCurrentMountChanged(object o, ValueEventArgs<MountType> e) => ToggleDodgeJump(e.Value == MountType.None);
+        private void OnIsInGameChanged(object o, ValueEventArgs<bool> e) => ToggleDodgeJump(e.Value);
+        private void OnIsTextInputFocusChanged(object o, ValueEventArgs<bool> e) => ToggleDodgeJump(!e.Value);
+        private void OnIsMapOpenChanged(object o, ValueEventArgs<bool> e) => ToggleDodgeJump(!e.Value);
+        private void ToggleDodgeJump(bool enabled)
+        {
+            DodgeJumpKeyBindingSetting.Value.BlockSequenceFromGw2 = enabled;
+            if (enabled)
+                DodgeJumpKeyBindingSetting.Value.Activated += OnDodgeJumpKeyActivated;
+            else
+                DodgeJumpKeyBindingSetting.Value.Activated -= OnDodgeJumpKeyActivated;
+        }
+
+        protected override void OnModuleLoaded(EventArgs e)
+        {
+            // Base handler must be called
+            base.OnModuleLoaded(e);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _autoClickController?.Update();
+        }
+
+        /// <inheritdoc />
+        protected override void Unload()
+        {
+            AutoClickToggleKeySetting.Value.Enabled = false;
+            AutoClickHoldKeySetting.Value.Enabled = false;
+            GameService.Gw2Mumble.PlayerCharacter.CurrentMountChanged -= OnCurrentMountChanged;
+            GameService.GameIntegration.Gw2Instance.IsInGameChanged -= OnIsInGameChanged;
+            GameService.Gw2Mumble.UI.IsTextInputFocusedChanged -= OnIsTextInputFocusChanged;
+            GameService.Gw2Mumble.UI.IsMapOpenChanged -= OnIsMapOpenChanged;
+            DodgeJumpKeyBindingSetting.Value.Enabled = false;
+            DodgeJumpKeyBindingSetting.Value.BlockSequenceFromGw2 = false;
+            _autoClickController?.Dispose();
+            DodgeJumpKeyBindingSetting.Value.Activated -= OnDodgeJumpKeyActivated;
+            AutoClickSoundVolume.SettingChanged -= OnAutoClickSoundVolumeSettingChanged;
+            _moduleIcon?.Dispose();
+            // All static members must be manually unset
+            ModuleInstance = null;
+        }
+    }
+}
